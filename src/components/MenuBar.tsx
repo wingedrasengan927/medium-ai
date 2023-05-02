39c71a795c8e462a2d53b46a16376bb483946a31
Macharla S Neeraj Krishna <ms.neerajkrishna@gmail.com>
2023-05-03 00:01:33 +0530

    redirect to github on click

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -3,7 +3,10 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 const MenuBar = () => {
     return (
         <div className="navbar bg-white">
-            <a className="btn text-black btn-ghost normal-case text-2xl">
+            <a
+                href="https://github.com/wingedrasengan927/lexical-medium-clone"
+                className="btn text-black btn-ghost normal-case text-2xl"
+            >
                 <GitHubIcon className="mr-2" />
                 {"GitHub"}
             </a>
